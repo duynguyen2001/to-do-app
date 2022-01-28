15f0b77a72f8dff2ad0559b6a4a35bd6427cfcea
kduyng71 <knguye71@uic.edu>
2022-01-28 11:27:04 -0600

    new cors policy

--- src/Structure/components/CRUD.js
+++ src/Structure/components/CRUD.js
@@ -1,36 +1,41 @@
 import axios from "axios";
+// For common config
+axios.defaults.headers.post["Content-Type"] = "application/json";
+const mainAxios = axios.create({
+  baseURL: 'https://tododatabase-4477.restdb.io/'
+});
 var options = { 
   headers: 
    { 'cache-control': 'no-cache',
-     'x-apikey': '43de6d653d125545232251985cf074b63e25f',
-     'content-type': 'application/json' },
-     json: true,
+     'x-apikey': '61f423a67e552722950171d5',
+     'content-type': 'application/json'},
+     json: true
      };
 
 function Read(setTodoList, option) {
-  axios
-    .get('/rest/tododatabase',  options)
+  mainAxios
+    .get('https://tododatabase-4477.restdb.io/rest/tododatabase',  options)
     .then((res) => {setTodoList(res.data);
   console.log("data in res : " + res.data)})
     .catch((err) => console.log(err));
 }
 function Create(newObject, refreshList) {
   console.log(newObject);
-  axios
-    .post('/rest/tododatabase', newObject, options)
+  mainAxios
+    .post('https://tododatabase-4477.restdb.io/rest/tododatabase', newObject, options)
     .then((res) => refreshList())
     .catch((err) => console.log(err));
 }
 function Update(item_id, dataChange, refreshList) {
   console.log(dataChange);
-  axios
-    .put(`/rest/tododatabase/${item_id}`, dataChange, options)
+  mainAxios
+    .put(`https://tododatabase-4477.restdb.io/rest/tododatabase/${item_id}`, dataChange, options)
     .then((res) => refreshList())
     .catch((err) => console.log(err));
 }
 function Delete(item, refreshList) {
-  axios
-    .delete(`/rest/tododatabase/${item._id}/`, options)
+  mainAxios
+    .delete(`https://tododatabase-4477.restdb.io/rest/tododatabase/${item._id}/`, options)
     .then((res) => refreshList())
     .catch((err) => console.log(err));
 }
