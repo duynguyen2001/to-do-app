e528fe364e4ccc7ec597b559a9c79fa32b9adadd
kduyng71 <kduyng71@gmail.com>
2022-01-28 03:23:27 -0600

    fix bugs

--- src/Structure/components/renderList.js
+++ src/Structure/components/renderList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Task from "./Task.js";
 
 export function renderList(TodoList, handleDelete, handleCompleted, handleEdit) {
-  return (TodoList).map((item) => (
+  return TodoList.map((item) => (
     <Task
       item={item}
       handleDelete={handleDelete}
